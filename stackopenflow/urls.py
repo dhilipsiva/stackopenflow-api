82089a2cfca84899c021f222a39c4ea4facdafa6
dhilipsiva <dhilipsiva@pm.me>
2021-05-21 11:50:21 +0530

    add a healthcheck url

--- stackopenflow/urls.py
+++ stackopenflow/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from graphene_django.views import GraphQLView
 from graphql_jwt.decorators import jwt_cookie
 
+from stackopenflow.core.views import health_check
+
 urlpatterns = [
     path(r"graphql", jwt_cookie(GraphQLView.as_view(graphiql=True))),
-    path("", include("social_django.urls", namespace="social")),
+    path("health_check", health_check),
     path("admin/", admin.site.urls),
 ]
